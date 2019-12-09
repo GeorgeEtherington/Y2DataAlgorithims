fdd735f02fb3c21a42147b367ca7939da38fc554
G.Etherington U1874339 <U1874339@unimail.hud.ac.uk>
2019-12-09 12:10:03 +0000

    Improved tests

--- week3/src/genericMethods/Swap.java
+++ week3/src/genericMethods/Swap.java
@@ -4,12 +4,20 @@ public class Swap {
 
     public static <T> T[] swap (T[] objectArray, int index1, int index2){
 
-        T holdVar;
-        holdVar = objectArray[index1];
-
-        objectArray[index1] = objectArray[index2];
-        objectArray[index2] = holdVar;
-
+        if (index1 < 0 || index1> objectArray.length){
+            throw new ArrayIndexOutOfBoundsException("Index1 must contain a value within the array");
+        }
+        if (index2 < 0 || index2> objectArray.length){
+            throw new ArrayIndexOutOfBoundsException("Index2 must contain a value within the array");
+        }
+        if (objectArray.length == 0)
+        {
+            throw new NullPointerException("objectArray must not be empty");
+        }
+            T holdVar;
+            holdVar = objectArray[index1];
+            objectArray[index1] = objectArray[index2];
+            objectArray[index2] = holdVar;
 
         return objectArray;
     }
--- week3/tests/genericMethods/SwapTest.java
+++ week3/tests/genericMethods/SwapTest.java
@@ -19,24 +19,24 @@ public class SwapTest {
         assertArrayEquals(compareArray, Swap.swap(intArray, 1, 2));
     }
 
-//    @Test
-//    void testNullArrayException() {
-//        assertThrows(NullPointerException.class, () ->
-//                Swap.swap(null, 0, 4));
-//    }
-//
-//    @Test
-//    void testOutOfBoundsException() {
-//        assertThrows(ArrayIndexOutOfBoundsException.class, () ->
-//                Swap.swap(stringArray, 1, 8));
-//    }
-//
-//
-//    @Test
-//    void testNegativeException() {
-//        assertThrows(ArrayIndexOutOfBoundsException.class, () ->
-//                Swap.swap(stringArray, -1, 4));
-//    }
+    @Test
+    void testNullArrayException() {
+        assertThrows(NullPointerException.class, () ->
+                Swap.swap(null, 0, 4));
+   }
+
+    @Test
+    void testOutOfBoundsException() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () ->
+                Swap.swap(stringArray, 1, 8));
+    }
+
+
+    @Test
+    void testNegativeException() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () ->
+                Swap.swap(stringArray, -1, 4));
+    }
 
 }
 
